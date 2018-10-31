24cac823a240ec2689169db3e115a08fd298af8f
Lukasz <luk.dutka@gmail.com>
2018-10-31 11:52:04 +0100

    działa wszystko w Przejazdach

--- app/src/main/java/com/example/lukasz/apptravel/db/dao/PrzejazdDao.java
+++ app/src/main/java/com/example/lukasz/apptravel/db/dao/PrzejazdDao.java
@@ -6,6 +6,7 @@ import android.arch.persistence.room.Query;
 
 import com.example.lukasz.apptravel.db.entities.Przejazd;
 
+import java.util.Date;
 import java.util.List;
 
 @Dao
@@ -22,5 +23,11 @@ public interface PrzejazdDao {
 
     @Query("DELETE FROM przejazd WHERE id=:id")
     void deletePrzejazdById(long id);
+
+    @Query("SELECT * FROM przejazd WHERE id=:id")
+    Przejazd getPrzejazdById(long id);
+
+    @Query("UPDATE przejazd SET kategoriaPrzejazduId=:kategoriaId, nazwa=:nazwa, dataOd=:data, koszt=:koszt WHERE id=:id")
+    void updatePrzejazd(long id, long kategoriaId, String nazwa, Date data, double koszt);
 
 }
