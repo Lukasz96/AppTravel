83539f8be800b14bb6d6911f68f9528e3bb82c69
Lukasz <luk.dutka@gmail.com>
2020-03-26 16:52:50 +0100

    w  podrozy są klucze obce nowych kategorii

--- app/src/main/java/com/my/lukasz/apptravel/db/entities/Podroz.java
+++ app/src/main/java/com/my/lukasz/apptravel/db/entities/Podroz.java
@@ -1,6 +1,7 @@
 package com.my.lukasz.apptravel.db.entities;
 
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
 import android.arch.persistence.room.PrimaryKey;
 import android.arch.persistence.room.TypeConverters;
 import android.support.annotation.NonNull;
@@ -10,11 +11,33 @@ import com.my.lukasz.apptravel.db.DateTypeConverter;
 import java.io.Serializable;
 import java.util.Date;
 
-@Entity
+import static android.arch.persistence.room.ForeignKey.CASCADE;
+
+@Entity(foreignKeys = {@ForeignKey(entity = User.class,
+        parentColumns = "id",
+        childColumns = "userId",
+        onDelete = CASCADE),
+        @ForeignKey(entity = KategoriaTransport.class,
+                parentColumns = "id",
+                childColumns = "kategoriaTransportuId",
+                onDelete = CASCADE),
+        @ForeignKey(entity = KategoriaWakacji.class,
+                parentColumns = "id",
+                childColumns = "kategoriaWakacjiId",
+                onDelete = CASCADE),
+        @ForeignKey(entity = KategoriaPogody.class,
+                parentColumns = "id",
+                childColumns = "kategoriaPogodyId",
+                onDelete = CASCADE)
+})
 public class Podroz implements Serializable {
 
     @PrimaryKey(autoGenerate = true)
     private long id;
+    private long userId;
+    private long kategoriaTransportuId;
+    private long kategoriaWakacjiId;
+    private long kategoriaPogodyId;
     @NonNull
     private String nazwa;
     @NonNull
@@ -77,12 +100,52 @@ public class Podroz implements Serializable {
         this.waluta = waluta;
     }
 
-    public Podroz(long id, @NonNull String nazwa, @NonNull Date dataOd, @NonNull Date dataDo, Double budzet, String waluta) {
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public long getKategoriaTransportuId() {
+        return kategoriaTransportuId;
+    }
+
+    public void setKategoriaTransportuId(long kategoriaTransportuId) {
+        this.kategoriaTransportuId = kategoriaTransportuId;
+    }
+
+    public long getKategoriaWakacjiId() {
+        return kategoriaWakacjiId;
+    }
+
+    public void setKategoriaWakacjiId(long kategoriaWakacjiId) {
+        this.kategoriaWakacjiId = kategoriaWakacjiId;
+    }
+
+    public long getKategoriaPogodyId() {
+        return kategoriaPogodyId;
+    }
+
+    public void setKategoriaPogodyId(long kategoriaPogodyId) {
+        this.kategoriaPogodyId = kategoriaPogodyId;
+    }
+
+    public void setBudzet(Double budzet) {
+        this.budzet = budzet;
+    }
+
+    public Podroz(long id, long userId, long kategoriaTransportuId, long kategoriaWakacjiId, long kategoriaPogodyId, @NonNull String nazwa, @NonNull Date dataOd, @NonNull Date dataDo, Double budzet, String waluta) {
         this.id = id;
+        this.userId = userId;
+        this.kategoriaTransportuId = kategoriaTransportuId;
+        this.kategoriaWakacjiId = kategoriaWakacjiId;
+        this.kategoriaPogodyId = kategoriaPogodyId;
         this.nazwa = nazwa;
         this.dataOd = dataOd;
         this.dataDo = dataDo;
         this.budzet = budzet;
-        this.waluta=waluta;
+        this.waluta = waluta;
     }
 }
